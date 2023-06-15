d0b437126971d5aee39d829a4866621e1560161f
dshilman <shilmantech@gmail.com>
2023-06-15 15:08:32 +0000

    new look and fee

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index1.html')
 
 
 @app.route('/sentiment', methods=['POST'])
