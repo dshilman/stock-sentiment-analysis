23f653b2e4d9b1b4a22adbe7d44053aaae64e700
dshilman <shilmantech@gmail.com>
2023-06-16 21:25:04 +0000

    stock prices filtering

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-import datetime
 import json
 import logging
 import os
+from datetime import datetime, timedelta
 
-# for graph plotting in website
-# NLTK VADER for sentiment analysis
 import nltk
+import nltk.sentiment.util
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
+import pytz
 import requests
 from dotenv import load_dotenv
 from flask import Flask, render_template, request
@@ -16,11 +17,10 @@ from plotly.utils import PlotlyJSONEncoder
 
 nltk.downloader.download('vader_lexicon')
 
-import nltk.sentiment.util
-from nltk.sentiment import SentimentAnalyzer
 
-api_url = os.getenv("API-URL")
-api_key = os.getenv("API-Key")
+api_url = os.getenv("API_URL")
+api_key = os.getenv("API_Key")
+
 api_host = os.getenv("RapidAPI-Host")
 
 # logging.basicConfig(filename='app_log.log',
@@ -31,6 +31,65 @@ app = Flask(__name__)
 if __name__ == '__main__':
     app.run(debug=True, port=8001)
 
+
+def convert_to_est_datetime(data_time_num):
+
+    utc_datetime = datetime.fromtimestamp(data_time_num, tz=pytz.utc)
+    est_datetime = utc_datetime.astimezone(
+        pytz.timezone('US/Eastern')).strftime("%b-%d-%y %I:%M%p")
+
+    return est_datetime
+
+
+def get_price_history_from_yahoo(ticker):
+
+    querystring = {"symbol": {ticker},
+                   "interval": "15m", "diffandsplits": "false"}
+    url_price = "https://mboum-finance.p.rapidapi.com/hi/history"
+
+    headers = {
+        "X-RapidAPI-Key": api_key,
+        "X-RapidAPI-Host": api_host
+    }
+
+    response = requests.get(url_price, headers=headers, params=querystring)
+
+    # with open(file='sample.json', mode='r', encoding="utf8") as myfile:
+    #     data = myfile.read()
+
+    respose_json = response.json()
+
+    price_history = respose_json['items']
+    data_dict = []
+
+    for stock_price in price_history.values():
+
+        date_time_num = stock_price["date_utc"]
+        est_date_time = convert_to_est_datetime(date_time_num)
+        price = stock_price["open"]
+        data_dict.append([est_date_time, price])
+
+
+    # Set column names
+    columns = ['datetime', 'price']
+    df = pd.DataFrame(data_dict, columns=columns)
+    df['datetime'] = pd.to_datetime(df['datetime'])
+    df = filter_by_date(df)
+    df.sort_values(by='datetime', ascending=False)
+    df.reset_index(inplace=True)
+    df.drop('index', axis=1, inplace=True)
+
+    return df
+
+def filter_by_date(df):
+
+    today = datetime.today()
+    day = timedelta(days = 1)
+    yesterday = today - day
+
+    df = df[df['datetime'].dt.date > yesterday.date]
+
+    return df
 
 def get_news_from_yahoo(ticker):
 
@@ -53,10 +112,11 @@ def get_news_from_yahoo(ticker):
 
     for article in articles:
         # Mon, 05 Jun 2023 20:46:19 +0000
-        datatime_i = datetime.datetime.strptime(
+        datatime_i_utc = datetime.strptime(
             article['pubDate'], '%a, %d %b %Y %H:%M:%S %z')
-        date_i_str = datatime_i.strftime("%b-%d-%y")
-        time_i_str = datatime_i.strftime("%I:%M%p")
+        est_datetime = datatime_i_utc.astimezone(pytz.timezone('US/Eastern'))
+        date_i_str = est_datetime.strftime("%b-%d-%y")
+        time_i_str = est_datetime.strftime("%I:%M%p")
 
         # date_time_i_str = datatime_i.strftime("%Y-%m-%d %H:%M:%S")
         title_i = article['title']
@@ -67,11 +127,10 @@ def get_news_from_yahoo(ticker):
     parsedata_df = pd.DataFrame(data_dict, columns=columns)
     parsedata_df['datetime'] = pd.to_datetime(
         parsedata_df['date'] + ' ' + parsedata_df['time'])
+
     parsedata_df.sort_values(by='datetime', ascending=False)
     parsedata_df.reset_index(inplace=True)
     parsedata_df.drop('index', axis=1, inplace=True)
-
-    logging.debug(parsedata_df)
 
     return parsedata_df
 
@@ -107,7 +166,7 @@ def plot_hourly_sentiment(parsed_and_scored_news, ticker):
 
     # Plot a bar chart with plotly
     fig = px.bar(mean_scores, x=mean_scores.index, y='sentiment_score',
-                 title=ticker + ' Hourly Sentiment Scores')
+                 title=f"{ticker} Hourly Sentiment Scores")
     return fig  # instead of using fig.show(), we return fig and turn it into a graphjson object for displaying in web page later
 
 
@@ -118,8 +177,16 @@ def plot_daily_sentiment(parsed_and_scored_news, ticker):
 
     # Plot a bar chart with plotly
     fig = px.bar(mean_scores, x=mean_scores.index,
-                 y='sentiment_score', title=ticker + ' Daily Sentiment Scores')
+                 y='sentiment_score', title=f"{ticker} Daily Sentiment Scores")
     return fig  # instead of using fig.show(), we return fig and turn it into a graphjson object for displaying in web page later
+
+
+def plot_hourly_price(df, ticker):
+
+    print(df)
+    fig = go.Figure([go.Scatter(x=df['datetime'], y=df['price'],
+                    title=f"{ticker} Daily Stock Price Performance")])
+    return fig
 
 
 app = Flask(__name__)
@@ -136,26 +203,24 @@ def sentiment():
     # logging.debug('In sentiment')
 
     ticker = request.form['ticker'].upper()
-
-    parsed_news_df = get_news_from_yahoo(ticker)
+    news_df = get_news_from_yahoo(ticker)
 
 
 ##############################################################
-    parsed_and_scored_news = score_news(parsed_news_df)
-    fig_hourly = plot_hourly_sentiment(parsed_and_scored_news, ticker)
-    fig_daily = plot_daily_sentiment(parsed_and_scored_news, ticker)
+    scored_news = score_news(news_df)
+    fig_hourly = plot_hourly_sentiment(scored_news, ticker)
+    fig_daily = plot_daily_sentiment(scored_news, ticker)
 
-    graphJSON_hourly = json.dumps(
-        fig_hourly, cls=PlotlyJSONEncoder)
+######################################################################
+
+    price_history_df = get_price_history_from_yahoo(ticker)
+    fig_price_history = plot_hourly_price(price_history_df, ticker)
+
+    graphJSON_hourly = json.dumps(fig_hourly, cls=PlotlyJSONEncoder)
     graphJSON_daily = json.dumps(fig_daily, cls=PlotlyJSONEncoder)
+    graphJSON_price = json.dumps(fig_price_history, cls=PlotlyJSONEncoder)
 
-    description = """
-	The above chart averages the sentiment scores of {} stock hourly and daily.
-	The table below gives each of the most recent headlines of the stock and the negative, neutral, positive and an aggregated sentiment score.
-	The news headlines are obtained from Mboum Finance API.
-    """.format(ticker)
-    
-    return render_template('sentiment.html', ticker=ticker, graphJSON_hourly=graphJSON_hourly, graphJSON_daily=graphJSON_daily, table=parsed_and_scored_news.to_html(classes='data'), description=description)
+    return render_template('sentiment.html', ticker=ticker, graphJSON_price=graphJSON_price, graphJSON_hourly=graphJSON_hourly, graphJSON_daily=graphJSON_daily, table=scored_news.to_html(classes='data'))
 
 
 if __name__ == '__main__':
