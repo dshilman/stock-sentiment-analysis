6c444ddcd72238c1ebf00f61df964bd16cfa4fb7
David Shilman <shilmantech@gmail.com>
2024-02-28 08:39:41 -0500

    tank

--- code/linux.sh
+++ code/linux.sh
@@ -5,3 +5,5 @@ mldir=~/tmp
 sudo dnf install cronie cronie-anacron
 cd stock-sentiment-analysis
 sudo TMPDIR=~/tmp/ python3 -m pip install -r requirements.txt --no-cache-dir
+cd code
+sudo python3 app.py
